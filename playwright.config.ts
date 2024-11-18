4b6dc6eeb31e9af743a8b0a643b765a751f3e0e8
fuston <fuston@James-Fuston-MacBook-Pro-M3-36-GIG.local>
2024-11-18 16:38:01 -0600

    framework and tests

---
+++ playwright.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './e2e/tests',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 3 : undefined,
+  reporter: 'html',
+  use: {
+    // we'd likely use an environment variable for baseurl in a real-world scenario
+    baseURL: 'https://weather.com',
+    // changing from default 'on-first-retry' so we don't lose the failure trace on a flaky test
+    trace: 'retain-on-failure',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+  ]
+});
